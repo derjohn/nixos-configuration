8c91d98bd01fa463ee0f0d3d0d9fa212afa17ed0
derjohn <himself@derjohn.de>
2023-07-10 14:06:34 +0200

    Moved home.nix to new location

--- copyconfigs.sh
+++ copyconfigs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cp -v ~/.config/nixpkgs/home.nix ./home/.config/nixpkgs/home.nix
+cp -v ~/.config/home-manager/home.nix ./home/.config/home-manager/home.nix
 for i in $(ls etc/nixos); do
   sudo cat /etc/nixos/${i} > ./etc/nixos/${i}
 done
