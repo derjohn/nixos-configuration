ea019445c749f16b9ae88ea564e9dd2c6e97a002
derjohn <himself@derjohn.de>
2024-11-01 13:38:55 +0100

    new java + k3s

--- etc/nixos/k3s.nix
+++ etc/nixos/k3s.nix
@@ -1,9 +1,14 @@
 {
   # This is required so that pod can reach the API server (running on port 6443 by default)
   networking.firewall.allowedTCPPorts = [ 6443 ];
-  services.k3s.enable = false;
-  services.k3s.role = "server";
-  services.k3s.extraFlags = toString [
+  services.k3s = {
+    enable = true;
+    role = "server";
+    extraFlags = toString [
     # "--kubelet-arg=v=4" # Optionally add additional args to k3s
-  ];
+    # "--cluster-cidr=10.42.0.0/16,2a10:3781:25ac:2::/64 --service-cidr=10.43.0.0/16,2a10:3781:25ac:3::/112 --flannel-iface eno1"
+    # "--no-deploy servicelb --no-deploy traefik --bind-address 192.168.51.1 --tls-san 192.168.51.1 --node-ip=192.168.51.1 --node-external-ip=192.168.51.1 --write-kubeconfig-mode 644"
+    ];
+  };
 }
+
