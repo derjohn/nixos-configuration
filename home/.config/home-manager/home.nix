ce3aa7e39a6bcc88b6b32f08f7b1ec1ca30344eb
derjohn <himself@derjohn.de>
2025-02-17 15:05:37 +0100

    Packages

--- home/.config/home-manager/home.nix
+++ home/.config/home-manager/home.nix
@@ -30,12 +30,13 @@ let
 #    includeEmulator = true;
 #    emulatorVersion = "30.9.0";
 #  };
-   temurin-bin-21-low = pkgs.temurin-bin-21.overrideAttrs(oldAttrs: { meta.priority = 5; });
-   temurin-bin-17-low = pkgs.temurin-bin-17.overrideAttrs(oldAttrs: { meta.priority = 10; });
-   temurin-bin-8-low = pkgs.temurin-bin-8.overrideAttrs(oldAttrs: { meta.priority = 10; });
+   temurin-bin-21-low = pkgs.temurin-bin-21.overrideAttrs(oldAttrs: { meta.priority = 21; });
+   temurin-bin-17-low = pkgs.temurin-bin-17.overrideAttrs(oldAttrs: { meta.priority = 17; });
+   temurin-bin-8-low = pkgs.temurin-bin-8.overrideAttrs(oldAttrs: { meta.priority = 8; });
 
    phpstuff = pkgs.php.withExtensions ({ enabled, all }: enabled ++ [ all.imagick ]);
    # phpstuff = (php.withExtensions ({ all, enabled }: enabled ++ (with all; [ imagick redis ]))).packages.composer;
+   # openjfx_jdk = pkgs.openjfx.override { withWebKit = true; };
 in
 
 {
@@ -45,7 +46,7 @@ in
   home.username = "aj";
   home.homeDirectory = "/home/aj";
   # pkgs.azure-functions-core-tools pkgs.azure-storage-azcopy
-  home.packages = [ pkgs._3proxy pkgs.adoptopenjdk-icedtea-web pkgs.age pkgs.akonadi pkgs.android-tools pkgs.ansible pkgs.appimage-run pkgs.asdf-vm pkgs.autojump pkgs.avrdude pkgs.aws-mfa pkgs.awscli2 pkgs.azure-cli pkgs.brave pkgs.bruno pkgs.btop pkgs.byzanz pkgs.pkgs.chntpw pkgs.dart pkgs.dfu-util pkgs.dfu-programmer unstable.delve pkgs.dialog pkgs.dos2unix pkgs.drill pkgs.du-dust pkgs.ebusd pkgs.envsubst pkgs.evince pkgs.ffmpeg-full pkgs.filezilla pkgs.firebase-tools pkgs.fluent-bit pkgs.fluxctl pkgs.freecad pkgs.freerdp pkgs.ghostscript pkgs.git-crypt pkgs.git-filter-repo pkgs.glibc pkgs.gmp pkgs.gnome-network-displays pkgs.go-ethereum pkgs.google-drive-ocamlfuse pkgs.gron pkgs.grpc-gateway pkgs.grpc-tools pkgs.grpcurl pkgs.gsctl pkgs.gsettings-desktop-schemas pkgs.handbrake pkgs.hugo pkgs.hidapi pkgs.hugs pkgs.hunspell pkgs.hunspellDicts.de-de pkgs.hunspellDicts.en-us pkgs.hunspellDicts.de_DE pkgs.hyphen pkgs.imagemagick pkgs.insomnia pkgs.ipmitool pkgs.jameica pkgs.kalendar pkgs.kazam pkgs.kcalc pkgs.kdePackages.kdenlive unstable.keepassxc pkgs.kphotoalbum pkgs.krusader pkgs.kteatime pkgs.ktorrent pkgs.lapce pkgs.libreoffice-fresh (lib.hiPrio pkgs.localsend) pkgs.mediathekview pkgs.monero-gui pkgs.mosh pkgs.mpv pkgs.mupdf pkgs.mycrypto pkgs.nextcloud-client pkgs.nix-bundle pkgs.nixpkgs-fmt pkgs.nodejs pkgs.oathToolkit pkgs.ocrmypdf pkgs.ollama pkgs.onlyoffice-bin pkgs.openshot-qt pkgs.openssl pkgs.outils pkgs.paperwork pkgs.pavucontrol pkgs.pdfarranger pkgs.pdfgrep pkgs.pdfsam-basic pkgs.pdsh pkgs.peek pkgs.pgadmin4-desktopmode pkgs.pinta pkgs.pkgsCross.avr.buildPackages.gcc pkgs.platformio pkgs.postgresql_16 pkgs.proto-contrib pkgs.protoc-gen-go pkgs.protoc-gen-go-grpc pkgs.protoc-gen-validate pkgs.pssh pkgs.pwgen pkgs.python3Full unstable.quba pkgs.qpdf pkgs.qrencode pkgs.rbenv pkgs.rclone pkgs.redocly-cli pkgs.regclient pkgs.regsync pkgs.ripgrep pkgs.rpi-imager pkgs.rpl pkgs.rpmextract pkgs.ruby unstable.rustdesk-flutter pkgs.siege pkgs.signal-desktop pkgs.simplex-chat-desktop pkgs.sipcalc pkgs.socat pkgs.sops pkgs.neochat pkgs.sshpass pkgs.sshuttle pkgs.stoken pkgs.swaks pkgs.thunderbird-128 pkgs.unetbootin pkgs.untrunc-anthwlock pkgs.usbtop pkgs.viu pkgs.vscodium pkgs.wine pkgs.winetricks pkgs.wireshark pkgs.x2goclient pkgs.xdotool pkgs.xorg.xhost pkgs.xsel pkgs.xsv pkgs.yarn2nix pkgs.ydotool pkgs.zip pkgs.zlib ferdiumLatest python3Packages.pip python3Packages.virtualenv temurin-bin-17-low temurin-bin-21-low (google-cloud-sdk.withExtraComponents [google-cloud-sdk.components.gke-gcloud-auth-plugin]) phpstuff ];
+  home.packages = [ pkgs._3proxy pkgs.adoptopenjdk-icedtea-web pkgs.age pkgs.akonadi pkgs.android-tools pkgs.ansible pkgs.appimage-run pkgs.arduino-cli pkgs.asdf-vm pkgs.autojump pkgs.avrdude pkgs.aws-mfa pkgs.awscli2 pkgs.azure-cli pkgs.brave pkgs.bruno pkgs.btop pkgs.byzanz pkgs.cheese pkgs.chntpw pkgs.neo-cowsay pkgs.dart pkgs.dfu-util pkgs.dfu-programmer unstable.delve pkgs.dialog pkgs.dos2unix pkgs.drill pkgs.du-dust pkgs.ebusd pkgs.envsubst pkgs.evince pkgs.ffmpeg-full pkgs.filezilla pkgs.firebase-tools pkgs.fluent-bit pkgs.fluxctl pkgs.freecad pkgs.freerdp pkgs.ghostscript pkgs.git-crypt pkgs.git-filter-repo pkgs.glibc pkgs.gmp pkgs.gnome-network-displays pkgs.go-ethereum pkgs.google-drive-ocamlfuse pkgs.gron pkgs.grpc-gateway pkgs.grpc-tools pkgs.grpcurl pkgs.gsctl pkgs.gsettings-desktop-schemas pkgs.handbrake pkgs.hugo pkgs.hidapi pkgs.hugs pkgs.hunspell pkgs.hunspellDicts.de-de pkgs.hunspellDicts.en-us pkgs.hunspellDicts.de_DE pkgs.hyphen pkgs.imagemagick pkgs.insomnia pkgs.ipmitool pkgs.jameica pkgs.kalendar pkgs.kazam pkgs.kcalc pkgs.kdePackages.kdenlive unstable.keepassxc pkgs.kphotoalbum pkgs.krusader pkgs.kteatime pkgs.ktorrent pkgs.lapce pkgs.libreoffice-fresh (lib.hiPrio pkgs.localsend) pkgs.mediathekview pkgs.monero-gui pkgs.mosh pkgs.mpv pkgs.mupdf pkgs.mycrypto pkgs.nextcloud-client pkgs.nix-bundle pkgs.nixpkgs-fmt pkgs.nodejs pkgs.oathToolkit pkgs.ocrmypdf pkgs.ollama pkgs.onlyoffice-bin pkgs.openjfx pkgs.openshot-qt pkgs.openssl pkgs.outils pkgs.paperwork pkgs.pavucontrol pkgs.pdfarranger pkgs.pdfgrep pkgs.pdfsam-basic pkgs.pdsh pkgs.peek pkgs.pgadmin4-desktopmode pkgs.pinta pkgs.pkgsCross.avr.buildPackages.gcc pkgs.platformio pkgs.postgresql_16 pkgs.proto-contrib pkgs.protoc-gen-go pkgs.protoc-gen-go-grpc pkgs.protoc-gen-validate pkgs.pssh pkgs.pwgen pkgs.python3Full unstable.quba pkgs.qpdf pkgs.qrencode pkgs.rbenv pkgs.rclone pkgs.redocly-cli pkgs.regclient pkgs.regsync pkgs.ripgrep pkgs.rpi-imager pkgs.rpl pkgs.rpmextract pkgs.ruby unstable.rustdesk-flutter pkgs.siege pkgs.signal-desktop pkgs.simplex-chat-desktop pkgs.sipcalc pkgs.socat pkgs.sops pkgs.neochat pkgs.sshpass pkgs.sshuttle pkgs.stoken pkgs.swaks pkgs.thunderbird-128 pkgs.unetbootin pkgs.untrunc-anthwlock pkgs.usbtop pkgs.viu pkgs.vscodium pkgs.wine pkgs.winetricks pkgs.wireshark pkgs.x2goclient pkgs.xdotool pkgs.xorg.xhost pkgs.xsel pkgs.xsv pkgs.yarn2nix pkgs.ydotool pkgs.zip pkgs.zlib ferdiumLatest python3Packages.pip python3Packages.virtualenv temurin-bin-17-low temurin-bin-21-low (google-cloud-sdk.withExtraComponents [google-cloud-sdk.components.gke-gcloud-auth-plugin]) phpstuff ];
   # unstable.hoppscotch
   # nix-env -f .nix-defexpr/channels/nixos-unstable -iA signal-desktop
   home.sessionPath = [
@@ -106,9 +107,9 @@ in
     # PYTHONPATH = "${python-with-my-packages}/${python-with-my-packages.sitePackages}";
     # ANDROID_SDK_ROOT = "${androidComposition}/libexec/android-sdk";
     # ANDROID_NDK_ROOT = "\${ANDROID_SDK_ROOT}/ndk-bundle";
-    JAVA_17_HOME = "${temurin-bin-17-low}";
-    JAVA_21_HOME = "${temurin-bin-21-low}";
-    JAVA_HOME = "${temurin-bin-21-low}";
+    JAVA_17_HOME = "${temurin-bin-17-low}/lib/openjdk";
+    JAVA_21_HOME = "${temurin-bin-21-low}/lib/openjdk";
+    JAVA_HOME = "${temurin-bin-21-low}/lib/openjdk";
     NIX_LD_LIBRARY_PATH = lib.makeLibraryPath [
       pkgs.stdenv.cc.cc
       pkgs.openssl
