f69b93d8c10ddde6db0e3a79bf626168d14cf6fc
derjohn <himself@derjohn.de>
2025-12-25 22:44:02 +0100

    More struggles

--- home/.config/home-manager/overlays/freerdp2.nix
+++ home/.config/home-manager/overlays/freerdp2.nix
@@ -5,7 +5,7 @@ let
     url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/22.05.tar.gz";
     sha256 = "0d643wp3l77hv2pmg2fi7vyxn4rwy0iyr8djcw1h5x72315ck9ik";
   }) {
-    system = prev.system;
+    stdenv.hostPlatform.system = prev.system;
   };
 in {
   # freerdp2pkg = oldpkgs.freerdp;
