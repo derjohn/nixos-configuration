fc5209d9e54ec6fe880130bb3233dc40d8db98ea
derjohn <himself@derjohn.de>
2026-09-08 15:13:50 +0200

    ferdium 7.23

--- home/.config/home-manager/home.nix
+++ home/.config/home-manager/home.nix
@@ -10,10 +10,10 @@ let
   #    x86_64-linux = "sha256-ODQKFjBa2riJY26aPaAfLzuCyLYkB5oYSxIE28nMmwY=";
   #    aarch64-linux = "sha256-CYHoTw6JUyU63iTd9tAbfWVnb48WcZgGtjthqnlAD8I=";
   #  }
-    version = "7.2.2";
+    version = "7.2.3";
     src = pkgs.fetchurl {
       url = "https://github.com/ferdium/ferdium-app/releases/download/v${version}/Ferdium-linux-${version}-amd64.deb";
-      sha256 = "sha256-zTJWVv7CHEH3Tg1bDqpxB+Kiraoli1jTvQ9hXn3MOWA=";
+      sha256 = "sha256-+KP107a8Tmr2qR8pH+gHuXqEqiC5ExPGXoYOqV1Urbo=";
     };
   });
 
